90367229fad8781b40ef29a7e598e83c1802d53c
MontyGoose <andy@square11.co.uk>
2015-01-12 17:20:29 +0000

    Added gulp and updated package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,17 @@
+var gulp = require('gulp');
+var mocha = require('gulp-mocha');
 
+gulp.task('default', function() {
+  // place code for your default task here
+});
+
+
+gulp.task('test', function() {
+  return gulp.src(['tests/*.spec.js'], { read: false })
+    .pipe(mocha({
+      reporter: 'spec',
+      globals: {
+        expect: require('chai').expect
+      }
+    }));
+});
