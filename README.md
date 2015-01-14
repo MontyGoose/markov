b75813bad79a00698d201f4b930d13e9e16503f2
MontyGoose <andy@square11.co.uk>
2015-01-14 15:20:38 +0000

    Updated constructor and test

--- README.md
+++ README.md
@@ -1,5 +1,55 @@
 # markov
-NodeJS and (optionally) MongoDB n-order Markov chains
+NodeJS n-order Markov chains
 
-## Coming Soon - January 2015
 
+## Usage
+
+```Javascript
+var Markov = require('markov');
+
+var markov = new Markov();
+
+// or optionally pass parameters
+var markov = new Markov({order:2, element:'word'});
+
+
+//Add element to chain  (if "word")
+markov.addToChain("audrey");
+markov.addToChain("markov");
+// or "sentence"
+markov.addToChain("The cat sat on the mat.");
+
+// or add array of element ("word")
+markov.addArrayToChain(["hello","audrey","markov"]);
+
+//or ("sentence")
+markov.addArrayToChain(["Fuzzy Wuzzy was a bear,","Fuzzy Wuzzy had no hair.","Fuzzy Wuzzy wasn't fuzzy was he?"]);
+
+
+// ask the chain to pass you back a new word/sentences based on you input
+markov.ask();
+
+// or optionally pass paraemeters
+markov.ask({seed:"T", min:3, max:10});  
+//passes back word/sentence starting with T, length of element minimum 3 - maximum 10.
+
+```
+
+## Options
+#### new Markov()
+parameter|type|default|description
+---|---|:---:|:---
+order|{integer}|2|Order of the Markov chain
+element|{string}|'word'|Type of elements in chain - either word, sentances, other
+
+####ask()
+parameter|type|default|description
+---|---|:---:|---
+seed|{string}|''|Seed element
+min|{integer}||Minimum length of reponse
+max|{integer}||Maximum length of reponse
+
+
+
+## Further details
+Check out the tests for more examples.
--- tests/markov.spec.js
+++ tests/markov.spec.js
@@ -3,32 +3,91 @@
 var expect = require('chai').expect;
 var Markov = require('../lib/index');
 
-var foo = 'bar';
+var somewords = require("./wordarray");
 
-describe('Markov', function() {
+describe('1st order Markov', function() {
 
-  var markov = new Markov(2);
-  var somewords = require("./wordarray");
+  var markov = new Markov({order:1});
   
-  it('should load words', function() {
-    markov.addArrayToChain(somewords);      
-    expect(foo).to.be.a('string')
-      
+  it('should return empty string if no chain', function() {
+    var response = markov.ask();      
+    expect(response).to.be.a('string');
+    expect(response).to.equal('');
   });
     
+  it('should for single entry return input string', function() {
+    markov.addToChain("markov");
+    var response = markov.ask();      
+    expect(response).to.be.a('string');
+    expect(response).to.equal('markov');
+  });
     
-  it('should return string when asked', function() {
+  it('should accept mutliple entries and return string', function() {
+    markov.addToChain("hello");
+    markov.addToChain("again");
+    markov.addToChain("andrey");
+    markov.addToChain("markov");
     var response = markov.ask();
-    console.log(response);
+    expect(response).to.be.a('string')
+  });
+
+});
+
+
+describe('2nd order Markov', function() {
+
+  var markov = new Markov({order:2});
+  var somewords = require("./wordarray");
+
+  it('should return empty string if no chain', function() {
+    var response = markov.ask();      
+    expect(response).to.be.a('string');
+    expect(response).to.equal('');
+  });
+    
+  it('should for single entry return input string', function() {
+    markov.addToChain("markov");
+    var response = markov.ask();      
+    expect(response).to.be.a('string');
+    expect(response).to.equal('markov');
+  });
+    
+  it('should accept mutliple entries and return string', function() {
+    markov.addToChain("hello");
+    markov.addToChain("again");
+    markov.addToChain("andrey");
+    markov.addToChain("markov");
+    var response = markov.ask();
     expect(response).to.be.a('string')
   });
     
-  it('should return string larger than 3 and less than 10', function() {
-    var response = markov.ask(3,10);
-    console.log(response);
-    expect(response).to.be.a('string')
-      
+});
+
+
+describe('1st order Markov (sentences)', function() {
+
+  var markov = new Markov({order:1,element:'sentence'});
+  
+  it('should return empty string if no chain', function() {
+    var response = markov.ask();      
+    expect(response).to.be.a('string');
+    expect(response).to.equal('');
   });
     
+  it('should for single entry return input string', function() {
+    markov.addToChain("The cat");
+    var response = markov.ask();      
+    expect(response).to.be.a('string');
+    expect(response).to.equal('The cat');
+  });
+    
+  it('should accept mutliple entries and return string', function() {
+    markov.addToChain("Fuzzy Wuzzy was a bear");
+    markov.addToChain("Fuzzy Wuzzy has no hair");
+    markov.addToChain("Fuzzy Wuzzy wasn't fuzzy was he");
+    markov.addToChain("The cat sat on the mat");      
+    var response = markov.ask();
+    expect(response).to.be.a('string')
+  });
     
 });
